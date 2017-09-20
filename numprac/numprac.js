efb2ca00cea8c1246f584727d9a9585f5adebf01
GitCoderr <allonsytoutdesuite@gmail.com>
2017-09-20 20:21:54 +0800

    fixed small bug

--- numprac/numprac.js
+++ numprac/numprac.js
@@ -12,8 +12,8 @@
 //   say new number
 
 var levelRanges = {
-	1: {min: 1,	max: 20},
-	2: {min: 21, max: 99},
+	1: {min: 1,	max: 9},
+	2: {min: 10, max: 99},
 	3: {min: 100,	max: 999},
 	4: {min: 1000, max: 9999},
 	5: {min: 10000, max: 99999},
@@ -26,7 +26,7 @@ var levelRanges = {
 	12: {min: 100000000000, max: 999999999999}
 }
 
-var currLevel = 4;
+var currLevel = 1;
 var trialNum = 0;
 var streak = 0;
 var score = 0;
@@ -50,11 +50,20 @@ var startBtn = document.querySelector(".start-btn");
 window.onload = init;
 
 function init() {
-	updateScoreboard(currLevel, streak, score);
 	setListeners();
 }
 
+function resetVars() {
+	currLevel =  1;
+	trialNum = 0;
+	streak = 0;
+	score = 0;
+	points = 0;
+	updateScoreboard(currLevel, streak, score);
+}
+
 function startGame() {
+	resetVars();
 	updateScoreboard(currLevel, streak, score);
 	clearDisplay();
 	sayRndNumber();
@@ -97,7 +106,6 @@ function checkNumber() {
 	} else {
 		calculatePoints();
 		deductPoints();
-//		display.innerHTML = "Incorrect";
 		reviewIncorrect();
 		streak = 0;
 	}
@@ -169,7 +177,7 @@ function playNumber(number) {
 		var millionremainder = number % 1000000;
 		var quotient = Math.floor(millionremainder / 1000);
 		var remainder = millionremainder % 1000;
-		filesToPlay = new Array(new Audio("numbers/en/" + millionquotient + ".mp3"), new Audio("numbers/en/million.mp3"), new Audio("numbers/en/" + quotient + ".mp3"), new Audio("numbers/en/thousand.mp3"), new Audio("numbers/en/" + remainder + ".mp3"));
+		filesToPlay = ["numbers/en/" + millionquotient + ".mp3", "numbers/en/million.mp3", "numbers/en/" + quotient + ".mp3", "numbers/en/thousand.mp3", "numbers/en/" + remainder + ".mp3"];
 	}
 	if (number > 999999999 && number < 1000000000000) {
 		var billionquotient = Math.floor(number / 1000000000);
@@ -178,7 +186,7 @@ function playNumber(number) {
 		var millionremainder = billionremainder % 1000000;
 		var quotient = Math.floor(millionremainder / 1000);
 		var remainder = millionremainder % 1000;
-		filesToPlay = new Array(new Audio("numbers/en/" + billionquotient + ".mp3"), new Audio("numbers/en/billion.mp3"), new Audio("numbers/en/" + millionquotient + ".mp3"), new Audio("numbers/en/million.mp3"), new Audio("numbers/en/" + quotient + ".mp3"), new Audio("numbers/en/thousand.mp3"), new Audio("numbers/en/" + remainder + ".mp3"));
+		filesToPlay = ["numbers/en/" + billionquotient + ".mp3", "numbers/en/billion.mp3", "numbers/en/" + millionquotient + ".mp3", "numbers/en/million.mp3", "numbers/en/" + quotient + ".mp3", "numbers/en/thousand.mp3", "numbers/en/" + remainder + ".mp3"];
 	}
 	i = 0;
 	playSnd();
@@ -203,140 +211,40 @@ function activateNumPad() {
 }
 
 
-//function playSnd() {
-//	audio.src = filesToPlay[i];
-//	audio.play();
-////	audio.addEventListener('ended', function() {
-////		i += 1;
-////		playFile();
-////	});
-//	for (i = 1; i < filesToPlay.length; i++) {
-//		audio.addEventListener("ended", function() {
-//			playFile(filesToPlay[i]);
-//		});
-//	}
-//}
-//
-//function playFile(soundFile) {
-//	audio.src = soundFile;
-//	audio.play();
-//	if (i == filesToPlay.length) {
-//		numberPad.classList.remove("disabled");
-//		filesToPlay = [];
-//		return;
-//	}
-//}
-	
 
-
-
-
-
-
-// Lets you enter numbers with the keyboard
-document.addEventListener("keydown", function (event) {
-		if (event.keyCode == 49) {
-			enterednumber = document.getElementById("guess").innerHTML + 1;
-			document.getElementById("guess").innerHTML = enterednumber;
-		} else if (event.keyCode == 50) {
-			enterednumber = document.getElementById("guess").innerHTML + 2;
-			document.getElementById("guess").innerHTML = enterednumber;
-		} else if (event.keyCode == 51) {
-			enterednumber = document.getElementById("guess").innerHTML + 3;
-			document.getElementById("guess").innerHTML = enterednumber;
-		} else if (event.keyCode == 52) {
-			enterednumber = document.getElementById("guess").innerHTML + 4;
-			document.getElementById("guess").innerHTML = enterednumber;
-		} else if (event.keyCode == 53) {
-			enterednumber = document.getElementById("guess").innerHTML + 5;
-			document.getElementById("guess").innerHTML = enterednumber;
-		} else if (event.keyCode == 54) {
-			enterednumber = document.getElementById("guess").innerHTML + 6;
-			document.getElementById("guess").innerHTML = enterednumber;
-		} else if (event.keyCode == 55) {
-			enterednumber = document.getElementById("guess").innerHTML + 7;
-			document.getElementById("guess").innerHTML = enterednumber;
-		} else if (event.keyCode == 56) {
-			enterednumber = document.getElementById("guess").innerHTML + 8;
-			document.getElementById("guess").innerHTML = enterednumber;
-		} else if (event.keyCode == 57) {
-			enterednumber = document.getElementById("guess").innerHTML + 9;
-			document.getElementById("guess").innerHTML = enterednumber;
-		} else if (event.keyCode == 48) {
-			enterednumber = document.getElementById("guess").innerHTML + 0;
-			document.getElementById("guess").innerHTML = enterednumber;
-		} else if (event.keyCode == 13) {
-			isMatch();
-		}
-}, true);
-
-
-
-function chooseLevel(level) {
-	document.getElementById('wrapperRange').style.display = "none";
-	document.getElementById('wrapperTest').style.display = "none";
-	document.getElementById('wrapperStart').style.display = "block";
-	if (level == 1) {
-		min = 1;
-		max = 20;
-		levelName = "Super Duper Easy";
-	} else if (level == 2) {
-		min = 1;
-		max = 100;
-		levelName = "Super Easy";
-	} else if (level == 3) {
-		min = 100;
-		max = 999;
-		levelName = "Pretty Easy";
-	} else if (level == 4) {
-		min = 1000;
-		max = 9999;
-		levelName = "Easy";
-	} else if (level == 5) {
-		min = 10000;
-		max = 99999;
-		levelName = "Slightly Challenging";
-	} else if (level == 6) {
-		min = 100000;
-		max = 999999;
-		levelName = "Challenging";
-	} else if (level == 7) {
-		min = 1000000;
-		max = 9999999;
-		levelName = "Very Challenging";
-	} else if (level == 8) {
-		min = 10000000;
-		max = 99999999;
-		levelName = "Kinda Hard";
-	} else if (level == 9) {
-		min = 100000000;
-		max = 999999999;
-		levelName = "Hard";
-	} else if (level == 10) {
-		min = 1000000000;
-		max = 9999999999;
-		levelName = "Really Hard";
-	} else if (level == 11) {
-		min = 10000000000;
-		max = 99999999999;
-		levelName = "Insane";
-	} else if (level == 12) {
-		min = 100000000000;
-		max = 999999999999;
-		levelName = "Impossible";
-	}
-	xlevel = level;
-}
-
-function startOver() {
-	location.reload();
-}
-
-function chooseLanguage(language) {
-	document.getElementById('wrapperLanguage').style.display = "none";
-	//	document.getElementById('wrapperMode').style.display = "block";
-	document.getElementById('wrapperRange').style.display = "block";
-	document.getElementById('wrapperRange').style.height = "1300px";
-	setLanguage = language;
-	score = 0;
-}
+// Lets you enter numbers with the keyboard - needs refactoring
+//document.addEventListener("keydown", function (event) {
+//		if (event.keyCode == 49) {
+//			enterednumber = document.getElementById("guess").innerHTML + 1;
+//			document.getElementById("guess").innerHTML = enterednumber;
+//		} else if (event.keyCode == 50) {
+//			enterednumber = document.getElementById("guess").innerHTML + 2;
+//			document.getElementById("guess").innerHTML = enterednumber;
+//		} else if (event.keyCode == 51) {
+//			enterednumber = document.getElementById("guess").innerHTML + 3;
+//			document.getElementById("guess").innerHTML = enterednumber;
+//		} else if (event.keyCode == 52) {
+//			enterednumber = document.getElementById("guess").innerHTML + 4;
+//			document.getElementById("guess").innerHTML = enterednumber;
+//		} else if (event.keyCode == 53) {
+//			enterednumber = document.getElementById("guess").innerHTML + 5;
+//			document.getElementById("guess").innerHTML = enterednumber;
+//		} else if (event.keyCode == 54) {
+//			enterednumber = document.getElementById("guess").innerHTML + 6;
+//			document.getElementById("guess").innerHTML = enterednumber;
+//		} else if (event.keyCode == 55) {
+//			enterednumber = document.getElementById("guess").innerHTML + 7;
+//			document.getElementById("guess").innerHTML = enterednumber;
+//		} else if (event.keyCode == 56) {
+//			enterednumber = document.getElementById("guess").innerHTML + 8;
+//			document.getElementById("guess").innerHTML = enterednumber;
+//		} else if (event.keyCode == 57) {
+//			enterednumber = document.getElementById("guess").innerHTML + 9;
+//			document.getElementById("guess").innerHTML = enterednumber;
+//		} else if (event.keyCode == 48) {
+//			enterednumber = document.getElementById("guess").innerHTML + 0;
+//			document.getElementById("guess").innerHTML = enterednumber;
+//		} else if (event.keyCode == 13) {
+//			isMatch();
+//		}
+//}, true);
